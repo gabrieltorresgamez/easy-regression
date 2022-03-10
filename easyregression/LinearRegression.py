58b66511fc3aa95066719804535d9cddc3092839
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2022-03-10 18:00:54 +0100

    quickfix print error

--- easyregression/LinearRegression.py
+++ easyregression/LinearRegression.py
@@ -10,10 +10,6 @@ class LinearRegression:
         param = [model.coef_[0], model.intercept_]
         r2 = model.score(x_r, y)
 
-        print("f(x) =", param[0], "* x +", param[1])
-        print("R2 =", r2)
-        print("")
-
         plt.title("Linear regression")
         plt.xlabel("x")
         plt.ylabel("y")
@@ -35,5 +31,5 @@ class LinearRegression:
         plt.ylabel("Frequency")
         plt.show()
 
-        out = "f(x) =" + str(param[0]) + "* x +" + str(param[1]) + "\n" + "R2 = " +  str(r2)
+        out = "f(x) = " + str(param[0]) + "* x +" + str(param[1]) + "\n" + "R2 = " +  str(r2)
         print(out)
